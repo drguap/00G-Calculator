03652dd86383fedb4845dfd49f53b9bf8ddb41e2
Montana <57641868+drguap@users.noreply.github.com>
2023-10-15 00:47:06 -0400

    added more comments & more javascript
    trying to create onClick events for functions and trying to brainstorm the logic behind the calculator.

---
+++ calculator/main.js
@@ -0,0 +1,55 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//let autoClear  = [];
+//let addition = numbers + numbers; 
+//let subtraction = numbers - numbers;
+//let division = numbers / numbers;
+//let multiplication = numbers * numbers;
+let display = [];
+
+// add an event listner to the buttons
+function autoClear() {
+	console.log('auto clear is clicked');
+}
+
+function addition() {
+	console.log('Addition is clicked');
+}
+function subtraction() {
+	console.log('Subtraction is clicked');
+}
+function division() {
+	console.log('Division is clicked');
+}
+function multiplication() {
+	console.log('Multiplication is clicked');
+}
+function one() {
+	console.log("one is clicked");
+}
+function two() {
+	console.log("two is clicked");
+}
+function three() {
+	console.log("three is clicked");
+}
+function four() {
+	console.log("four is clicked");
+}
+function five() {
+	console.log("five is clicked");
+}
+function six() {
+	console.log("six is clicked");
+}
+function seven() {
+	console.log("seven is clicked");
+}
+function eight() {
+	console.log("eight is clicked");
+}
+function nine() {
+	console.log("nine is clicked");
+}
+function ten() {
+	console.log("ten is clicked");
+}
